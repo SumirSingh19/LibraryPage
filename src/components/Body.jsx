bd5748dbe8460f05914102b990ee00f3c7a0c26a
SumirSingh19 <134550739+SumirSingh19@users.noreply.github.com>
2024-04-26 14:01:28 +0530

    new files

---
+++ src/components/Body.jsx
@@ -0,0 +1,167 @@
+const Body = () => {
+    return (
+        <div className="ml-72 pb-10">
+            <div className="flex flex-col self-center px-5 mt-16 max-w-full w-[1100px] max-md:mt-10">
+      <div className="text-3xl font-semibold leading-[48.44px] text-neutral-700 max-md:max-w-full">
+        Welcome to DBRAU, Central Library
+      </div>
+      <div className="mt-8 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+          <div className="flex flex-col w-[66%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow max-md:mt-10 max-md:max-w-full">
+              <div className="text-base leading-7 text-black max-md:max-w-full">
+                The heart of any academic institution lies within its library
+                and the quality of its literary resources. Dr. B. R. Ambedkar
+                University prides itself on its exceptional library services.
+                The university boasts a Central Library located at the Paliwal
+                Campus, extending to 3 more campus libraries including Sanskriti
+                Bhawan at Civil lines, Chalesar Campus and Ambedkar Bhawan at
+                Khandari Campus. These facilities ensure that students have
+                access to a vast array of academic materials, fostering an
+                environment conducive to learning and research. As it embraces
+                the digital age, the Central Library remains at the forefront of
+                innovation, continually enhancing its offerings. Plans for
+                future upgrades include digitization, implementation of RFID
+                technology, and expansion of e-resources. With its dedication to
+                academic excellence and adaptability to changing times, the
+                Central Library stands as a beacon of intellectual curiosity and
+                discovery.
+                <br />
+                Establishment: The Central Library was established in 1927
+                within the university premises and later relocated to its
+                current building in 1953.
+              </div>
+              <div className="mt-24 text-xl font-semibold leading-8 text-neutral-700 max-md:mt-10 max-md:max-w-full">
+                More about Central Library:
+              </div>
+              <div className="mt-10 text-base leading-5 text-neutral-800 max-md:mt-10 max-md:max-w-full">
+                <span className="font-medium text-rose-800">
+                  Library Space:
+                </span>
+                <br />
+                <span className="text-neutral-800">
+                  Spanning an impressive 2500 square meters across three floors,
+                  the Central Library provides ample space for academic
+                  endeavors.
+                </span>
+                <br />
+                <br />
+                <span className="font-medium text-rose-800">
+                  Reading Room:{" "}
+                </span>
+                <br />
+                <span className="text-neutral-800">
+                  With dedicated reading halls totaling 350 square meters,
+                  students and scholars have tranquil spaces to engage with
+                  their studies.
+                </span>
+                <br />
+                <br />
+                <span className="font-medium text-rose-800">
+                  Total Collection:{" "}
+                </span>
+                <br />
+                <span className="text-neutral-800">
+                  The library houses a comprehensive collection comprising
+                  1,67,800 items, including approximately 1,40,000 books, 14,000
+                  doctoral dissertations, and 12,000 bound back volumes of
+                  periodicals. Additionally, patrons benefit from access to
+                  full-text electronic journals from seven renowned publishers,
+                  alongside approximately 100 gifted journals.
+                </span>
+                <br />
+                <br />
+                <span className="font-medium text-rose-800">
+                  Operating Hours:{" "}
+                </span>
+                <br />
+                <span className="text-neutral-800">
+                  Except for 2nd Saturdays, Sundays, and government holidays,
+                  the library remains open throughout the year, ensuring
+                  continuous access to its resources.
+                </span>
+                <br />
+                <br />
+                <span className="font-medium text-rose-800">
+                  Book Issuance:{" "}
+                </span>
+                <br />
+                <span className="text-neutral-800">
+                  The library operates a structured system for book issuance,
+                  with varying quotas and durations based on membership
+                  categories, ranging from faculty to retired teachers.
+                </span>
+                <br />
+                <br />
+                <span className="font-medium text-rose-800">
+                  Public Library Facilities:{" "}
+                </span>
+                <br />
+                <span className="text-neutral-800">
+                  Since 1998, the Central Library of the University has expanded
+                  its services beyond Institute and Affiliated Colleges to
+                  encompass individuals from society at large, offering
+                  membership to anyone seeking to utilize its resources.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-[34%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col mt-3.5 text-sm font-semibold leading-6 text-center text-neutral-700 text-opacity-70 max-md:mt-10">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/eb76b67e91c149db2efc30bfa70581f4c0e38707c66c49482e9d1aef28cc4cf1?apiKey=90a6ed213758449eabd6c6a72ae84f75&"
+                className="w-full aspect-[1.79]"
+              />
+              <div className="self-center mt-4">
+                Click here to know more about Central Library
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-16 max-md:mt-10 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+          <div className="flex flex-col w-[63%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow mt-2 text-xl leading-8 text-neutral-700 max-md:mt-10 max-md:max-w-full">
+              <div className="text-zinc-800 max-md:max-w-full">
+                Library Timings
+              </div>
+              <div className="mx-14 mt-7 text-base leading-7 max-md:mr-2.5 max-md:max-w-full">
+                Library is open from 10:00 AM to 5:00 PM Night from Monday to
+                Saturday. On Second
+                <br />
+                Saturday/ Sunday and Holidays it is off. 
+              </div>
+              <div className="mt-4 text-base leading-7 max-md:max-w-full">
+                The Library remains closed on National Holidays, Gazetted
+                Holidays and other holidays declared
+                <br />
+                by the University.
+              </div>
+              <div className="mt-9 text-rose-800 max-md:max-w-full">
+                Collection at a Glance
+              </div>
+              <div className="mt-10 text-rose-800 max-md:max-w-full">
+                Library Rules and Regulation
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-[37%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col text-sm leading-6 text-center text-neutral-700 text-opacity-70 max-md:mt-10">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/f254f0d5dec06793e6d85193bcb45ec8f34be4d57fd6d12d554b6188e9d5d1ec?apiKey=90a6ed213758449eabd6c6a72ae84f75&"
+                className="w-full aspect-[1.79]"
+              />
+              <div className="self-center mt-4">Reading hall</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+    )
+};
+
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,159 @@
+import { useState, useRef } from 'react';
+import ARROW from "../assets/icons/arrow-down.png";
+
+const Header = () => {
+    const [dropdowns, setDropdowns] = useState({
+        libraryProfile: false,
+        libraryServices: false,
+        eResources: false,
+        eventsActivities: false,
+    });
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const timeoutRef = useRef(null);
+
+    const handleMouseEnter = () => {
+        setIsDropdownOpen(true);
+        clearTimeout(timeoutRef.current);
+    };
+
+    const handleMouseLeave = () => {
+        timeoutRef.current = setTimeout(() => {
+            setIsDropdownOpen(false);
+        }, 200);
+    };
+
+    const handleDropdownMouseEnter = () => {
+        clearTimeout(timeoutRef.current);
+    };
+
+    const handleDropdownMouseLeave = () => {
+        timeoutRef.current = setTimeout(() => {
+            setIsDropdownOpen(false);
+        }, 200);
+    };
+
+    const toggleDropdown = (dropdown) => {
+        setDropdowns((prevDropdowns) => ({
+            ...prevDropdowns,
+            [dropdown]: !prevDropdowns[dropdown],
+        }));
+    };
+
+    return (
+        <div className="flex bg-gradient-to-r from-[#03045E] to-[#023E8A] justify-center items-center gap-10">
+            <div>
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/04e0cd858c6ffd5b766231770be65e10b1f4961c0ac31b9a23eae301b694cbad?apiKey=90a6ed213758449eabd6c6a72ae84f75"
+                    className="shrink-0 max-w-full aspect-[1.03] w-[115px]"
+                />
+            </div>
+            <div className="flex flex-col gap-2">
+                <div className="text-white">
+                    <ul className="flex gap-20">
+                        <li className="relative flex items-center">
+                            <span>Library Profile</span>
+                            <img
+                                src={ARROW}
+                                className="w-3 h-3 ml-1 arrow-icon cursor-pointer"
+                                onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}
+                            />
+                            {dropdowns.libraryProfile && (
+                                <div className="dropdown-menu">
+                                    <div className='bg-white text-black w-48'>
+                                        <ul className='flex flex-col'>
+                                            <li>About Library</li>
+                                            <li>Vision & Mission</li>
+                                            <li>Library Rules</li>
+                                            <li>Library Facilities</li>
+                                            <li>Collection at glance</li>
+                                            <li>Library Committee</li>
+                                            <li>M.O.U</li>
+                                            <li>Library Staff</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            )}
+                        </li>
+                        <li className="relative flex items-center">
+                            <span>Library Services</span>
+                            <img
+                                src={ARROW}
+                                className="w-3 h-3 ml-1 arrow-icon cursor-pointer"
+                                onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}
+                            />
+                            {dropdowns.libraryServices && (
+                                <div className="dropdown-menu">
+                                    <div className='bg-white text-black w-48'>
+                                        <ul className='flex flex-col'>
+                                            <li>Library Service</li>
+                                            <li>Research Support</li>
+                                            <li>E-News Papers & Clippings</li>
+                                            <li>University Rare Books</li>
+                                            <li>Collection at glance</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            )}
+                        </li>
+                        <li className="relative flex items-center">
+                            <span>E-resources</span>
+                            <img
+                                src={ARROW}
+                                className="w-3 h-3 ml-1 arrow-icon cursor-pointer"
+                                onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}
+                            />
+                            {dropdowns.eResources && (
+                                <div className="dropdown-menu">
+                                    <div className='bg-white text-black w-48'>
+                                        <ul className='flex flex-col'>
+                                            <li>Libtrend</li>
+                                            <li>Open Access E-Books</li>
+                                            <li>Open Access E-Resources</li>
+                                            <li>E-Magazines</li>
+                                            <li>E-Database</li>
+                                            <li>INDCAT</li>
+                                            <li>Delnet Connectivity</li>
+                                            <li>South Asia Archive(SAA)</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            )}
+                        </li>
+                        <li className="relative flex items-center">
+                            <span>Events and Activities</span>
+                            <img
+                                src={ARROW}
+                                className="w-3 h-3 ml-1 arrow-icon cursor-pointer"
+                                onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}
+                            />
+                            {dropdowns.eventsActivities && (
+                                <div className="dropdown-menu">
+                                    <div className='bg-white text-black w-48'>
+                                        <ul className='flex flex-col'>
+                                            <li>Delnet Workshop</li>
+                                            <li>Librarian Day Celebration</li>
+                                            <li>Journal Publication</li>
+                                            <li>Library Facilities</li>                                       
+                                        </ul>
+                                    </div>
+                                </div>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+                <div className="text-white pl-10">
+                    <ul className="flex gap-20">
+                        <li className="cursor-pointer">Downloads</li>
+                        <li className="cursor-pointer">Gallery</li>
+                        <li className="cursor-pointer">Other Links</li>
+                        <li className="cursor-pointer">FAQs</li>
+                        <li className="cursor-pointer">Contact Us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
